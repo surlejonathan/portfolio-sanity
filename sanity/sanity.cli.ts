ca1be5332eb006e03d0a51f06011e71b56dea663
surlejonathan <surle.jonathan@gmail.com>
2023-01-19 19:43:12 +0100

    keep credentials secret

--- sanity/sanity.cli.ts
+++ sanity/sanity.cli.ts
@@ -2,7 +2,7 @@ import {defineCliConfig} from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
-    projectId: 'w6x6i1vp',
-    dataset: 'production'
-  }
+    projectId: process.env.SANITY_PROJECT_ID,
+    dataset: process.env.SANITY_DATASET,
+  },
 })
